430d6889224171b7d013a48d46d2d23a93729571
nvsclub <nvsclub.w@gmail.com>
2018-05-05 15:03:36 +0100

    minor fixes population_structure

--- population_structure.py
+++ population_structure.py
@@ -57,10 +57,11 @@ class Object:
   def generate_grid(self):
     self.grid = [[0 for i in range(self.set.length)] for j in range(self.set.width)]
 
-    for i in range(self.y, self.y + self.width):
-      for j in range(self.x, self.x + self.length):
+    for i in range(self.y, self.y + self.length):
+      for j in range(self.x, self.x + self.width):
+        print(i,j)
         self.grid[i][j] = 1
-
+        
   def move(self, dx, dy):
     if dx + self.x >= self.set.width or dy + self.y >= self.set.length or dx + self.x < 0 or dy + self.y < 0:
       return 
