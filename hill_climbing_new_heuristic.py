7b42015e2377006121847c6e7a6c707a3767904e
Dany199612 <up201405902@fe.up.pt>
2018-05-18 18:43:10 +0100

    finnished hill_new_heuristic

--- hill_climbing_new_heuristic.py
+++ hill_climbing_new_heuristic.py
@@ -6,32 +6,39 @@ import ui_windows as ui
 
 def list_successors(set):
     successors = []
-    for i in range(0, len(set.items)):
-        successors.append(new_successor(set, 1, 0, i))
-        successors.append(new_successor(set, -1, 0, i))
-        successors.append(new_successor(set, 0, 1, i))
-        successors.append(new_successor(set, 0, -1, i))
-        successors.append(new_successor_rotate(set, i))
+    for i in range(0, len(set.items) - 1):
+        for x in range(i + 1, len(set.items)):
+            successors.append(new_successor(set,i,x))
     return successors
 
-def new_successor(set, dx, dy, item):
-    successor = copy.deepcopy(set)
-    successor.items[item].move(dx, dy)
-    successor.generate_representation()
-    successor.calculate_heuristic()
-    return successor
+def new_successor(set, item_1, item_2):
+    sucessor = copy.deepcopy(set)
+    sucessor.items[item_2], sucessor.items[item_1] = sucessor.items[item_1], sucessor.items[item_2]
+    sucessor.place_objects()
+    return sucessor
 
+def find_best(successors):
+    best = successors[0]
+    ui.print_div('SUCCESSORS')
+    for successor in successors:
+        ui.print_set(successor)
+        if successor.heuristic >= best.heuristic:
+            best = successor
+    return best
 
 def main(grid_height, grid_width, requested_objects):
     set = ps.Set(grid_height, grid_width, requested_objects, False)
     set.place_objects()
-    set.generate_representation()
+    print("set inicial:")
+    print("heuristica: " + str(set.heuristic))
     ui.print_set(set)
-    '''while(True):
+    while (True):
         successors = list_successors(set)
         best = find_best(successors)
-        ui.print_set(best)
         if (set.heuristic < best.heuristic):
             set = best
         else:
-            break'''
+            break
+    print("novo set: ")
+    ui.print_set(set)
+    print("heuristica: " + str(best.heuristic))
